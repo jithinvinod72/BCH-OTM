d8a1c71340ee695a0faf8dda0a983c54d07d7573
jibin joseph <jibin8086@gmail.com>
2022-12-01 20:48:21 +0530

    Added new Controller and etc

---
+++ BCMCH.OTM.API/BCMCH.OTM.Domain/Booking/BookingDomainService.cs
@@ -0,0 +1,13 @@
+﻿using BCMCH.OTM.Domain.Contract.Booking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCMCH.OTM.Domain.Booking
+{
+    public class BookingDomainService : IBookingDomainService
+    {
+    }
+}
