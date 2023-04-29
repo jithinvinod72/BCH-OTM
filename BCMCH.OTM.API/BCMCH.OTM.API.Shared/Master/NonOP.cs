3a76a5d489acb299268a2444af41a150b6349ba1
Jithin Vinod <72181381+jithinvinod72@users.noreply.github.com>
2023-04-29 17:54:00 +0530

    Create NonOP.cs

---
+++ BCMCH.OTM.API/BCMCH.OTM.API.Shared/Master/NonOP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BCMCH.OTM.API.Shared.Master
+{
+	public class NonOP
+	{
+        public string patientUHID { get; set; }
+        public int procedure { get; set; }
+        public int priority { get; set; }
+        public string provisionalDiagnosis { get; set; }
+        public string comments { get; set; }
+
+    }
+}
+
